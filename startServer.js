161220732971b572d6a405c3b1212b690bbcc4b6
narayank97 <narayankarun@gmail.com>
2019-11-13 15:25:49 -0800

    added database code

--- startServer.js
+++ startServer.js
@@ -5,6 +5,26 @@ const PORT = process.env.PORT || 3000;
 var http = require('http');
 var server = http.Server(app);
 
+//***************************This part establishes Database****************************************
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
+  //*******************This part deals with the routing********************************************
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/home.html');
     console.log(`We hit home page`);
